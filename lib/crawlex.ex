85454df8eb2ee50ea357537088557403e4d6d830
bigfang <bitair@gmail.com>
2017-03-09 21:20:28 +0800

    init

---
+++ lib/crawlex.ex
@@ -0,0 +1,18 @@
+defmodule Crawlex do
+  @moduledoc """
+  Documentation for Crawlex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Crawlex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crawlex_test.exs
@@ -0,0 +1,8 @@
+defmodule CrawlexTest do
+  use ExUnit.Case
+  doctest Crawlex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
