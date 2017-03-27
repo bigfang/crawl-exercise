9320a90df28403c2c55216ef098f062ca759665b
bigfang <bitair@gmail.com>
2017-03-27 16:59:09 +0800

    生成表单供连续抓取

--- lib/crawlex.ex
+++ lib/crawlex.ex
@@ -5,11 +5,15 @@ defmodule Crawlex do
 
   alias Crawlex.{Fetcher, Parser}
 
-  def run do
-    Fetcher.fetch
+  def work(arg) do
+    Fetcher.fetch(arg)
     |> Parser.parse
     |> IO.inspect
+  end
 
-    nil
+  def run do
+    init_form = [mddid: 10035, pageid: "mdd_index", sort: 2, cost: 0, days: 0, month: 0, tagid: 0, page: 0]
+    forms = for n <- 1..3, do: Keyword.put(init_form, :page, n)
+    Enum.map(forms, &work/1)
   end
 end
--- lib/fetcher.ex
+++ lib/fetcher.ex
@@ -6,8 +6,7 @@ defmodule Crawlex.Fetcher do
   @url "http://www.mafengwo.cn/gonglve/ajax.php?act=get_travellist"
   @headers %{"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36"}
 
-  def fetch do
-    form = [mddid: 10035, pageid: "mdd_index", sort: 2, cost: 0, days: 0, month: 0, tagid: 0, page: 1]
+  def fetch(form)do
     case HTTPoison.post(@url, {:form, form}, @headers) do
       {:ok, %{status_code: 200, body: body}} ->
         body
