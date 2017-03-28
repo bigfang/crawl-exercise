17462e62321c3cfda7580f8587790f6938b62063
bigfang <bitair@gmail.com>
2017-03-28 09:54:19 +0800

    抓取中打印日志

--- lib/crawlex.ex
+++ lib/crawlex.ex
@@ -3,18 +3,21 @@ defmodule Crawlex do
   Documentation for Crawlex.
   """
 
+  require Logger
   alias Crawlex.{Fetcher, Parser, Storer}
 
   def work(arg) do
-    Fetcher.fetch(arg)
-    |> Parser.parse
-    |> Storer.store
-    |> IO.inspect
+    res = Fetcher.fetch(arg)
+          |> Parser.parse
+          |> Storer.store
+    Logger.info("Page #{Keyword.get(arg, :page)}: Store #{length(res)} items")
   end
 
   def run do
+    Logger.info("Crawler is Starting!")
     init_form = [mddid: 10035, pageid: "mdd_index", sort: 2, cost: 0, days: 0, month: 0, tagid: 0, page: 0]
     forms = for n <- 1..3, do: Keyword.put(init_form, :page, n)
     Enum.map(forms, &work/1)
+    Logger.info("Crawler Finished!")
   end
 end
--- lib/fetcher.ex
+++ lib/fetcher.ex
@@ -3,14 +3,18 @@ defmodule Crawlex.Fetcher do
   Fetcher module of Crawlex.
   """
 
+  require Logger
+
   @url "http://www.mafengwo.cn/gonglve/ajax.php?act=get_travellist"
   @headers %{"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36"}
 
   def fetch(form)do
     case HTTPoison.post(@url, {:form, form}, @headers) do
       {:ok, %{status_code: 200, body: body}} ->
+        Logger.info("Page #{Keyword.get(form, :page)}: Fetch SUCCESS!")
         body
       _ ->
+        Logger.info("Page #{Keyword.get(form, :page)}: Fetch FAIlED!")
         false
     end
   end
