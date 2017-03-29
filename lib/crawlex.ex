4179832b9afe36004c6445d55df759bd5d7416aa
bigfang <bitair@gmail.com>
2017-03-29 10:50:30 +0800

    在管道中使用记录log

--- lib/crawlex.ex
+++ lib/crawlex.ex
@@ -7,16 +7,18 @@ defmodule Crawlex do
   alias Crawlex.{Fetcher, Parser, Storer}
 
   def work(arg) do
-    res = Fetcher.fetch(arg)
-          |> Parser.parse
-          |> Storer.store
-    Logger.info("Page #{Keyword.get(arg, :page)}: Store #{length(res)} items")
+    Fetcher.fetch(arg)
+    |> (fn(x) -> Logger.info("Page #{Keyword.get(arg, :page)}: Parse Starting..."); x end).()
+    |> Parser.parse
+    |> (fn(x) -> Logger.info("Page #{Keyword.get(arg, :page)}: Parse #{length(x)} items"); x end).()
+    |> Storer.store
+    |> (fn(x) -> Logger.info("Page #{Keyword.get(arg, :page)}: Store #{length(x)} items") end).()
   end
 
   def run do
     Logger.info("Crawler is Starting!")
     init_form = [mddid: 10035, pageid: "mdd_index", sort: 2, cost: 0, days: 0, month: 0, tagid: 0, page: 0]
-    forms = for n <- 1..3, do: Keyword.put(init_form, :page, n)
+    forms = for n <- 1..2, do: Keyword.put(init_form, :page, n)
     Enum.map(forms, &work/1)
     Logger.info("Crawler Finished!")
   end
