fb659214c01d638be581ef2ef4519625ddc44a64
bigfang <bitair@gmail.com>
2017-04-06 15:56:57 +0800

    尝试ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Crawlex.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     name: "Crawler-exercise",
+     docs: [main: "readme",
+            extras: ["README.md"]]]
   end
 
   # Configuration for the OTP application
@@ -33,6 +36,7 @@ defmodule Crawlex.Mixfile do
     [{:httpoison, "~> 0.11.0"},
      {:poison, "~> 3.0"},
      {:floki, "~> 0.15.0"},
-     {:amnesia, "~> 0.2.7"}]
+     {:amnesia, "~> 0.2.7"},
+     {:ex_doc, "~> 0.15", only: :dev, runtime: false}]
   end
 end
